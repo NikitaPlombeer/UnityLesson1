45096fe87443d08162307adf88987ab5775c9556
Plombeer <francuzov72@gmail.com>
2021-08-08 12:46:25 +0200

    Add materials

---
+++ Assets/Scripts/CastleBlock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleBlock : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void turnOnPhysics()
+    {
+        rigidbody.isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/CastleDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleDestroyer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Domino"))
+        {
+            foreach (CastleBlock castleBlock in FindObjectsOfType<CastleBlock>())
+            {
+                castleBlock.turnOnPhysics();
+            }
+        }
+    }
+}
